e885ad328d2d51ccb6fa21c3eb525f5f8830b5ef
Yinxue Li <me@liyinxue.com>
2023-12-04 16:51:06 -0800

    add math solution

---
+++ 1688. Count of Matches in Tournament/C#.cs
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=1688 lang=csharp
+ *
+ * [1688] Count of Matches in Tournament
+ */
+
+// @lc code=start
+public class Solution {
+    public int NumberOfMatches(int n) {
+        return n - 1;
+    }
+}
+// @lc code=end
+
